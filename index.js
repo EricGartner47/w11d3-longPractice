4fe57bb8ebcbffa6cfcc171013b5fe054b6ea108
Daniel Flores <daniel.flores4930@gmail.com>
2021-11-03 13:01:30 -0700

    phase 2

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const csrf = require('csurf');
+const { urlencoded } = require("express");
+const csrfProtection = csrf({ cookie: true });
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "pug");
-
+app.use(cookieParser());
+app.use(express.urlencoded({ extends: false }));
 
 const users = [
   {
@@ -19,6 +24,21 @@ app.get("/", (req, res) => {
   res.render('index', {users});
 });
 
+app.get("/create", csrfProtection, (req, res) => {
+  res.render('create-form', { csrfToken: req.csrfToken() });
+});
+
+app.post("/create", (req, res) => {
+  const { firstName, lastName, email, password, confirmedPassword } = req.body;
+  res.render('create-form', {
+    firstName,
+    lastName,
+    email,
+    password,
+    confirmedPassword
+  });
+  return;
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
